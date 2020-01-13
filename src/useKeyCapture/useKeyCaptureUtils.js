6dc24dbc8548cfb349f78957660823ca2d5813de
Pranesh <praneshpsg239@gmail.com>
2020-01-13 12:44:01 +0530

    added modifier keys and refactor

--- src/useKeyCapture/useKeyCaptureUtils.js
+++ src/useKeyCapture/useKeyCaptureUtils.js
@@ -1,22 +1,34 @@
 import { useCallback } from 'react';
 
-export const useKeyInitialState = {
-  keyCode: null,
-  keyName: null,
+export const initialState = {
+  // Pressed key
+  key: null,
+
   isEscape: false,
   isEnter: false,
   isSpace: false,
-  isArrows: false,
-  isRightArrow: false,
-  isLeftArrow: false,
-  isUpArrow: false,
-  isDownArrow: false,
+
+  // Arrow keys
+  isArrow: false,
+  isArrowRight: false,
+  isArrowLeft: false,
+  isArrowUp: false,
+  isArrowDown: false,
+
   isTab: false,
+
+  // Modifier keys
   isWithShift: false,
   isWithCtrl: false,
+  isWithMeta: false,
+  isWithAlt: false,
+
   isNumber: false,
+
+  // Character varients
   isCaps: false,
   isSmall: false,
+
   isSpecialCharacter: false
 };
 
@@ -31,27 +43,40 @@ export const useKeyActionTypes = {
   ARROWS: 'ARROWS'
 };
 
-const arrowKeysMapper = {
-  24: 'UpArrow',
-  25: 'DownArrow',
-  26: 'RightArrow',
-  27: 'LeftArrow'
+const modifierKeys = {
+  ctrlKey: 'Ctrl',
+  shiftKey: 'Shift',
+  altKey: 'Alt',
+  metaKey: 'Meta'
 };
 
 const keyCodeMapper = {
   13: useKeyActionTypes.ENTER_KEY,
   27: useKeyActionTypes.ESCAPE_KEY,
-  32: useKeyActionTypes.Error
+  32: useKeyActionTypes.SPACE
 };
 
-const getArrowKeysPayload = keyCode => {
+const getArrowKeysPayload = key => {
   return {
-    [`is${arrowKeysMapper[keyCode]}`]: true
+    [`is${key}`]: true
   };
 };
 
 const isCapitalLetterPressed = key => /^[A-Z]$/.test(key);
 const isSmallLetterPressed = key => /^[a-z]$/.test(key);
+
+const getModifierPayload = eventDetails => {
+  let modifierPayloadObj = {};
+  const modifierObjKeys = Object.keys(modifierKeys);
+
+  for (let key of modifierObjKeys) {
+    if (eventDetails[key]) {
+      modifierPayloadObj[`isWith${modifierKeys[key]}`] = true;
+    }
+  }
+
+  return modifierPayloadObj;
+};
 
 /**
  * Returns the action type for the key pressed
@@ -63,33 +88,41 @@ export const getAction = eventDetails => {
     throw new Error('Event called with no details');
   }
 
-  if (keyCodeMapper[eventDetails.keyCode]) {
-    return { type: useKeyActionTypes.SPACE };
-  }
-
-  if (keyCodeMapper[eventDetails.keyCode])
-    return { type: keyCodeMapper[eventDetails.keyCode] };
-
-  if (isCapitalLetterPressed(eventDetails.key)) {
-    return { type: useKeyActionTypes.CAPS_ALPHABET };
-  }
-
-  if (isSmallLetterPressed(eventDetails.key)) {
-    return { type: useKeyActionTypes.SMALL_ALPHABET };
-  }
-
-  if (eventDetails.keyCode >= 48 && eventDetails.keyCode <= 57) {
-    return { type: useKeyActionTypes.NUMBER };
-  }
-
   if (eventDetails.keyCode >= 24 && eventDetails.keyCode <= 27) {
     return {
       type: useKeyActionTypes.ARROWS,
-      payload: getArrowKeysPayload(eventDetails.keyCode)
+      payload: {
+        ...getArrowKeysPayload(eventDetails.key),
+        ...getModifierPayload(eventDetails),
+        key: eventDetails.key
+      }
     };
   }
 
-  return { type: 'SOME_OTHER_KEY' };
+  let type;
+
+  if (keyCodeMapper[eventDetails.keyCode])
+    type = keyCodeMapper[eventDetails.keyCode];
+
+  if (isCapitalLetterPressed(eventDetails.key)) {
+    type = useKeyActionTypes.CAPS_ALPHABET;
+  }
+
+  if (isSmallLetterPressed(eventDetails.key)) {
+    type = useKeyActionTypes.SMALL_ALPHABET;
+  }
+
+  if (eventDetails.keyCode >= 48 && eventDetails.keyCode <= 57) {
+    type = useKeyActionTypes.NUMBER;
+  }
+  if (!type) {
+    type = 'SOME_OTHER_KEY';
+  }
+
+  return {
+    type,
+    payload: { ...getModifierPayload(eventDetails), key: eventDetails.key }
+  };
 };
 
 export const useEnhancedReducer = reducer =>
@@ -99,8 +132,3 @@ export const useEnhancedReducer = reducer =>
     },
     [reducer]
   );
-
-// {
-//   payload: event,
-//   type: getActionType(event)
-// }
