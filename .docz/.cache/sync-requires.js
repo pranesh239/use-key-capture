5aef050218a0aaa287fbb33157e0d83395b24153
pranesh239 <praneshpsg239@gmail.com>
2020-02-09 10:18:05 +0530

    added README and updated docz

--- .docz/.cache/sync-requires.js
+++ .docz/.cache/sync-requires.js
@@ -5,8 +5,10 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/pranesh.g/Documents/opensource/use-key-capture-2/.docz/.cache/dev-404-page.js"))),
   "component---docs-index-mdx": hot(preferDefault(require("/Users/pranesh.g/Documents/opensource/use-key-capture-2/docs/index.mdx"))),
   "component---docs-pages-demo-mdx": hot(preferDefault(require("/Users/pranesh.g/Documents/opensource/use-key-capture-2/docs/pages/demo.mdx"))),
+  "component---docs-pages-props-mdx": hot(preferDefault(require("/Users/pranesh.g/Documents/opensource/use-key-capture-2/docs/pages/props.mdx"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/pranesh.g/Documents/opensource/use-key-capture-2/.docz/src/pages/404.js")))
 }
 
